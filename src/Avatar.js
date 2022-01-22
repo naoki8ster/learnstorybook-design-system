abd791b5a038d5acc55c78c1e3da69effd7605b9
Naoki Yamazaki <naoki8ster@gmail.com>
2022-01-22 21:08:26 +0900

    after chapter6

--- src/Avatar.js
+++ src/Avatar.js
@@ -98,6 +98,10 @@ const Initial = styled.div`
   `}
 `;
 
+/**
+- Use an avatar for attributing actions or content to specific users.
+- The user's name should always be present when using Avatar – either printed beside the avatar or in a tooltip.
+**/
 export function Avatar({ loading, username, src, size, ...props }) {
   let avatarFigure = <Icon icon="useralt" />;
   const a11yProps = {};
@@ -124,6 +128,9 @@ export function Avatar({ loading, username, src, size, ...props }) {
 }
 
 Avatar.propTypes = {
+  /**
+  Use the loading state to indicate that the data Avatar needs is still loading.
+  */
   loading: PropTypes.bool,
   username: PropTypes.string,
   src: PropTypes.string,
